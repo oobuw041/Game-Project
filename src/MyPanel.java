914661294f080d1255b75f42a5d849a993bf87f4
keno <keno.obuwoma@gmail.com>
2024-08-20 15:03:13 -0400

    added the enemy  and its functionsto the frame

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,31 +1,32 @@
+
+
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.event.*;
-
-
 import javax.swing.*;
 
 public class MyPanel extends JPanel implements ActionListener{
-  final int WIDTH  = 500;
-  final int HEIGHT = 500;
+  final int WIDTH  = 600;
+  final int HEIGHT = 600;
   
   Image enemy;
   Image backgroundImage;
   Timer timer;
   
-  int xVelocity;
-  int yVelocity;
+  int xVelocity = 7;
+  int yVelocity = 1;
   int x = 0;
   int y = 0;
   
   
 	public MyPanel() {
 		this.setPreferredSize(new Dimension(WIDTH,HEIGHT));	
+		this.setBackground(Color.black);
 		enemy = new ImageIcon("enemy.png").getImage();
-		backgroundImage = new ImageIcon("sky.jpg").getImage();
+		//backgroundImage = new ImageIcon("space.jpg").getImage();
 		timer = new Timer(1,this);
 		timer.start();
 	}
@@ -34,34 +35,29 @@ public class MyPanel extends JPanel implements ActionListener{
 	 public void paint (Graphics g) {
 		  super.paint(g);
 		  Graphics2D graphics = (Graphics2D)g;
-		  graphics.drawImage(backgroundImage,0, 0, null);
 		  graphics.drawImage(enemy, x, y, null);
+		  graphics.drawImage(backgroundImage, 0, 0, null);
+		 
 	  }
 
 	 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(x>= WIDTH - enemy.getWidth(null)) {
-			xVelocity = xVelocity * - 1;
-		
+	 @Override
+		public void actionPerformed(ActionEvent e) {
+			if(x >= WIDTH - enemy.getWidth(null)|| x< 0) {
+				 xVelocity = xVelocity * -1;
+				repaint();
+				
+			}
+			x = x + xVelocity;
+			repaint();
 			
-		}
-		
-		x +=xVelocity;
-		repaint();
-		
-		
-		if(y>= WIDTH - enemy.getWidth(null)) {
-			yVelocity = yVelocity * - 1;
-		
+	 }}	
 			
-		}
 		
-		y +=yVelocity;
-		repaint();
-		
-	}
 
 	
 
-}
+
+	
+
+
